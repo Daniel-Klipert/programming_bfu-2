2fa449cc4d5fc5d43bce128c276258b359f1fc76
Daniel-Klipert <klipertdaniel02@gmail.com>
2026-01-20 21:56:19 +0200

    DEL qtsample

--- qt_sample_app/ext/qt-6.12.0-win10-22H2-x64-msvc-194/lib/cmake/Qt6Gui/Qt6QWindowsIntegrationPluginTargetsPrecheck.cmake
+++
@@ -1,8 +0,0 @@
-
-_qt_internal_should_include_targets(
-    TARGETS QWindowsIntegrationPlugin
-    NAMESPACE Qt6::
-    PROJECT_NAME QtBase
-    OUT_VAR_SHOULD_SKIP __qt_QWindowsIntegrationPlugin_skip_include_targets_file
-    
-)
