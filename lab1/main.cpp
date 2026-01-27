a2f00c75d96adde89c692194371fef0c16098db4
Daniel-Klipert <klipertdaniel02@gmail.com>
2026-01-27 19:24:36 +0200

    FIX lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,12 +1,22 @@
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 class Country
 {
+private:
+    string name_;
+    string capital_;
+    string date_;
+    int territory_;
+    int population_;
 
 public:
     Country() = default;
-    ~Country() = default;
+    ~Country() {
+        cout << "Run Dtor" << endl;
+    }
 
     Country(string name, string capital, string date, int territory, int population) : name_(name), capital_(capital), date_(date), territory_(territory), population_(population){}
     Country(const Country& other){
@@ -23,28 +33,45 @@ public:
     int get_territory() const {return territory_;}
     int get_population() const {return population_;}
 
-    void set_capital(const string& capital) {capital_ = capital;}
+    void set_capital(const string& capital) {
+        bool allowed = false;
+        if(capital.length() != 0){
+            allowed = true;
+            for (int i = 0; i < capital.length(); ++i){
+                char s = capital[i];
+                if (!('A' <= s && s <= 'Z' || 'a' <= s && s <= 'z')){
+                    allowed = false;
+                    break;
+                }
+            }
+        }
+        if (allowed){
+            capital_ = capital;
+        }
+        else{ cout << "----Wrong capital----" << endl;}
+    }
 
     void info(){
-        cout << "Название: " + name_ << endl;
-        cout << "Столица: " + capital_ << endl;
-        cout << "Дата основания: " + date_ << endl;
-        cout << "Площадь: " + to_string(territory_) << endl;
-        cout << "Населние: " + to_string(population_) << endl;
+        cout << "Name: " + name_ << endl;
+        cout << "Capital: " + capital_ << endl;
+        cout << "Date: " + date_ << endl;
+        cout << "Territory: " + to_string(territory_) << endl;
+        cout << "Population: " + to_string(population_) << endl;
     }
     void increase_territory(int territory){territory_ += territory;}
-    void increase_population(int growth){population_ += growth;}
+    void increase_territory(Country& other, float k = 1.0){
+        territory_ += other.territory_ * k;
+        other.territory_ = other.territory_ * (1 - k);
+    }
+    void increase_population(){
+        srand(time(0));
+        population_ += rand() % 100000;
+    }
     
-private:
-    string name_;
-    string capital_;
-    string date_;
-    int territory_;
-    int population_;
 };
 
 int main(){
-
+    
     Country China;
     Country Russia("Russian Federation", "Moscow", "25.12.1991", 17234031, 146203613);
     Country USA(Russia);
@@ -57,10 +84,19 @@ int main(){
     cout << endl;
     
     Russia.increase_territory(Russia.get_territory());
-    USA.increase_population(-1*USA.get_population());
+    USA.increase_population();
+    USA.set_capital("Washington");
+    China.set_capital("Beijing");
     
     Russia.info();
     cout << endl;
     USA.info();
+    cout << endl;
+    China.info();
 
+    Russia.increase_territory(USA, 0.5);
+
+    Russia.info();
+    cout << endl;
+    USA.info();
 }
